4e3809fd7ea90adfe6f5e25d1dd34417eacb4bea
thezj <thezj@qq.com>
2018-03-31 14:35:26 +0800

    forms events update remove

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,6 +2,9 @@ import React, {
     Component
 } from 'react'
 
+// import ReactDOM from 'react-dom'
+
+
 import {withTracker} from 'meteor/react-meteor-data'
 //这里就是连接到数据库返回了一个collection
 //所有对数据操作通过 对Tasks.find Tasks.insert...这些api来实现
@@ -13,6 +16,22 @@ import Task from './Task.js'
 
 class App extends Component {
 
+    handleSubmit(e){
+        e.preventDefault()
+        //find the text field via the react ref
+        //如果使用html接口 querySelector（class）这样如果页面有多个组件，则会出现错误选择
+        // let text = ReactDOM.findDOMNode(this.refs.textInput).value.trim()
+        let text = this.textInput.value.trim()
+
+        Tasks.insert({
+            text,
+            createdAT:new Date()
+        })
+
+        //clear form
+        this.textInput.value = ''
+    }
+
     renderTasks(){
         return this.props.tasks.map(task=><Task key={task._id} task={task}></Task>)
     }
@@ -23,6 +42,9 @@ class App extends Component {
                 <header>
                     <h1>todo list</h1>
                 </header>
+                <form className='new-task' onSubmit={e=>this.handleSubmit(e)}>
+                    <input type='text' ref={dom => this.textInput = dom} placeholder='type to add new tasks' />
+                </form>
                 <ul>
                     {this.renderTasks()}
                 </ul>
@@ -33,6 +55,6 @@ class App extends Component {
 
 export default withTracker(_=>{
     return {
-    tasks:Tasks.find({text:/11/}).fetch()
+    tasks:Tasks.find({text:/./},{sort:{createdAT:-1}}).fetch()
     }
 })(App)
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -2,10 +2,68 @@ import React, {
     Component
 } from 'react'
 
-export default class App extends Component {
+import {Tasks} from '../api/tasks'
+
+export default class Task extends Component {
+
+    format(time, format){
+        var t = new Date(time);
+        var tf = function(i){return (i < 10 ? '0' : '') + i};
+        return format.replace(/yyyy|MM|dd|HH|mm|ss/g, function(a){
+            switch(a){
+                case 'yyyy':
+                    return tf(t.getFullYear());
+                    break;
+                case 'MM':
+                    return tf(t.getMonth() + 1);
+                    break;
+                case 'mm':
+                    return tf(t.getMinutes());
+                    break;
+                case 'dd':
+                    return tf(t.getDate());
+                    break;
+                case 'HH':
+                    return tf(t.getHours());
+                    break;
+                case 'ss':
+                    return tf(t.getSeconds());
+                    break;
+            }
+        })
+    }
+
+    //set the checked property to the opposite of its current value
+    toggleChecked(){
+        //修改接口返回的collection中对应的数据
+        Tasks.update(this.props.task._id,{
+            $set:{
+                checked:!this.props.task.checked
+            }
+        })
+    }
+
+    //delete current task
+    deleteThisTask(){
+        //删除接口返回的collection中对应的数据
+        Tasks.remove(this.props.task._id)
+    }
+    
     render(){
         return (
-            <li>{this.props.task.text}</li>
+            //give tasks a different classname when they are checked off
+            //so that we can style them nicely in css
+            <li className={this.props.task.checked ? 'checked' : ''}>
+                <button className='delete' onClick={event => this.deleteThisTask(event)}>&times;</button>
+                <input
+                    type='checkbox'
+                    readOnly
+                    checked={!!this.props.task.checked}
+                    onClick={e=>this.toggleChecked(e)}
+                />
+
+                {this.props.task.text}<br />{this.format(this.props.task.createdAT, 'yyyy-MM-dd  HH:mm:ss')}
+            </li>
         )
     }
 }
